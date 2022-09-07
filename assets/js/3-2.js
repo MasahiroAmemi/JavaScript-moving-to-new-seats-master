2462d2867c512f1b0a90a2227f0efc5d801f3413
MASAHIRO AMEMIYA <ame6masa6@gmail.com>
2022-09-07 16:30:34 +0900

    assets/

---
+++ assets/js/3-2.js
@@ -0,0 +1,20 @@
+const fukuoka = "福岡県";
+document.querySelector("#fukuoka").innerHTML = fukuoka;
+ 
+const oita = "大分県";
+document.querySelector("#oita").innerHTML = oita;
+ 
+const miyazaki = "宮崎県";
+document.querySelector("#miyazaki").innerHTML = miyazaki;
+ 
+const kumamoto = "熊本県";
+document.querySelector("#kumamoto").innerHTML = kumamoto;
+ 
+const saga = "佐賀県";
+document.querySelector("#saga").innerHTML = saga;
+ 
+const nagasaki = "長崎県";
+document.querySelector("#nagasaki").innerHTML = nagasaki;
+ 
+const kagoshima = "鹿児島県";
+document.querySelector("#kagoshima").innerHTML = kagoshima;
