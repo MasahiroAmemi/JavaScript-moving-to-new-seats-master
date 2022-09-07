27acc848eb63be9cdab5a9deae85b7f7aa930f85
MASAHIRO AMEMIYA <ame6masa6@gmail.com>
2022-09-07 16:24:31 +0900

    assets/

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,6 @@ for(let i = 1; i <= 18; i++){
   insertHTML += '<div class="seat__item">' + i + '</div>';
 }
  document.querySelector('#seat').innerHTML = insertHTML;
+
+ const classMember = ["こすげ", "さとう", "たなか"];
+ console.log(classMember[0]);
