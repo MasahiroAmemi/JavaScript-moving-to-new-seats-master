a4ce047cb504c49f374ab646f87997a31c0bb079
MASAHIRO AMEMIYA <ame6masa6@gmail.com>
2022-09-08 15:51:21 +0900

    assets/

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const studentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 let insertHTML = ''; // 足し算した結果を保存して置く場所を確保する
  
- for(let i = 18; i > 0; i--){
+for(let i = studentNumbers.length; i > 0; i--){
    const randomNum = Math.floor(Math.random() * i);
    let tmp = studentNumbers[i - 1]; 
    studentNumbers[i - 1] = studentNumbers[randomNum]; 
