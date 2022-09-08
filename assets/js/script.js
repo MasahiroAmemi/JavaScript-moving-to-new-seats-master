19936c4133ac3f1a84468a874fcba2453f199336
MASAHIRO AMEMIYA <ame6masa6@gmail.com>
2022-09-08 15:50:23 +0900

    assets/

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
-conststudentNumbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
-let insertHTML = ''; 
+const studentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+let insertHTML = ''; // 足し算した結果を保存して置く場所を確保する
+ 
+ for(let i = 18; i > 0; i--){
+   const randomNum = Math.floor(Math.random() * i);
+   let tmp = studentNumbers[i - 1]; 
+   studentNumbers[i - 1] = studentNumbers[randomNum]; 
+   studentNumbers[randomNum] = tmp;
+ }
+  
+ console.log(studentNumbers);
 
-for(let i = 1; i <= 18; i++){
-  studentNumbers.forEach(function(num){
-    insertHTML += '<div class="seat__item">' + num + '</div>';
-  })
- document.querySelector('#seat').innerHTML = insertHTML;
+studentNumbers.forEach(function(num){
+  insertHTML += '<div class="seat__item">' + num + '</div>';
+})
 
- const classMember = ["こすげ", "さとう", "たなか"];
- console.log(classMember[0]);
+document.querySelector('#seat').innerHTML = insertHTML;
